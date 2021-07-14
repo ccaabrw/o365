7a169fdf51a7acf9ab4e01d63f05772aaf0ff814
Brian Watts <b.watts@ucl.ac.uk>
2021-07-14 09:04:12 +0100

    Add connect-admin

---
+++ connect-admin.ps1
@@ -0,0 +1,15 @@
+# Connect to O365
+# https://docs.microsoft.com/en-us/powershell/exchange/connect-to-exchange-online-powershell?view=exchange-ps
+#
+
+# Check module installed
+
+Import-Module ExchangeOnlineManagement
+# MAy not be explicitly required
+
+# Check if already connected
+
+#Connect-ExchangeOnline -UserPrincipalName 'ccaabrw@ucl.ac.uk'
+
+Connect-ExchangeOnline -UserPrincipalName 'ccaabrw-admin@live.ucl.ac.uk'
+
