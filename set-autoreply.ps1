404401ba8b85b32255e716e9518ad547c8ccfd96
Brian Watts <ccaabrw@ucl.ac.uk>
2022-06-01 16:23:01 +0000

    Update

--- set-autoreply.ps1
+++ set-autoreply.ps1
@@ -1,15 +1,17 @@
 # Call connect-exchange (that script determines whether it needs to actually make a new connection)
 
 # start date
-$d1 = New-Object System.DateTime(2022,4,10,17,0,0)
+$d1 = New-Object System.DateTime(2022,6,1,16,0,0)
 
 # end date
-$d2 = New-Object System.DateTime(2022,4,19,6,0,0)
+$d2 = New-Object System.DateTime(2022,6,13,6,0,0)
 
 Set-MailboxAutoReplyConfiguration -Identity ccaabrw -AutoReplyState scheduled `
   -StartTime $d1 -EndTime $d2 `
-  -InternalMessage "I am on leave until 19th April" -ExternalMessage $null
+  -InternalMessage "I am on leave until 13th June" -ExternalMessage $null
 
+# Use 4PM start time (is it UTC?) - also seems to take time to effect
+#
 # Parameterise this properly
 # Add a whatif and verbose option
 # Response is derived from expression according to end date
