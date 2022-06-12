3bd4638d6a6e575a3a5ccdc79a6bad46792dbd92
Brian Watts <ccaabrw@ucl.ac.uk>
2022-06-12 08:45:58 +0000

    add

--- set-autoreply.ps1
+++ set-autoreply.ps1
@@ -2,16 +2,22 @@
 
 # start date
 $d1 = New-Object System.DateTime(2022,6,1,16,0,0)
+$d1 = New-Object System.DateTime(2022,6,12,16,0,0)
 
 # end date
-$d2 = New-Object System.DateTime(2022,6,13,6,0,0)
+$d2 = New-Object System.DateTime(2022,6,17,6,0,0)
+
+# message
+$msg = "I am on leave until 13th June"
+$msg = "I am on a training course until 17th June"
 
 Set-MailboxAutoReplyConfiguration -Identity ccaabrw -AutoReplyState scheduled `
   -StartTime $d1 -EndTime $d2 `
-  -InternalMessage "I am on leave until 13th June" -ExternalMessage $null
+  -InternalMessage $msg -ExternalMessage $null
 
-# Use 4PM start time - since Cloud Shell is in UTC (?)
+# Use 4PM start time - since Cloud Shell is in UTC
 #
 # Parameterise this properly
 # Add a whatif and verbose option
 # Response is derived from expression according to end date
+# Handle local time zone
