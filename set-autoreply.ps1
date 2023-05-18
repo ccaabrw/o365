fb64b8536dd98edb4bcb7648c5de7e4e5e4a02a2
Brian Watts <ccaabrw@ucl.ac.uk>
2023-05-18 21:08:39 +0000

    update

--- set-autoreply.ps1
+++ set-autoreply.ps1
@@ -3,13 +3,29 @@
 # start date
 $d1 = New-Object System.DateTime(2022,6,1,16,0,0)
 $d1 = New-Object System.DateTime(2022,6,12,16,0,0)
+$d1 = New-Object System.DateTime(2022,9,19,16,0,0)
+$d1 = New-Object System.DateTime(2022,10,20,16,0,0)
+$d1 = New-Object System.DateTime(2022,12,23,16,0,0)
+$d1 = New-Object System.DateTime(2023,3,16,16,0,0)
+$d1 = New-Object System.DateTime(2023,3,23,16,0,0)
+$d1 = New-Object System.DateTime(2023,4,5,16,0,0)
+$d1 = New-Object System.DateTime(2023,4,27,16,0,0)
+$d1 = New-Object System.DateTime(2023,5,17,12,0,0)
 
 # end date
-$d2 = New-Object System.DateTime(2022,6,17,6,0,0)
+$d2 = New-Object System.DateTime(2023,5,18,0,0,0)
 
 # messages
 $msg = "I am on leave until 13th June"
 $msg = "I am on a training course until 17th June"
+$msg = "I am on leave until 21st September"
+$msg = "I am on leave until 25th October"
+$msg = "I am on leave until 9th January"
+$msg = "I am on leave until 21st March"
+$msg = "I am on leave until 27th March"
+$msg = "I am on leave until 17th April"
+$msg = "I am on leave until 9th May"
+$msg = "I am on leave this afternoon"
 
 Set-MailboxAutoReplyConfiguration -Identity ccaabrw -AutoReplyState scheduled `
   -StartTime $d1 -EndTime $d2 `
